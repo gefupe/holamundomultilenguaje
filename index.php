0450b75a7908fc3f40e32c405f651e54eb8b6675
gefupe <gefupe@alumnatflorida.es>
2025-05-09 21:26:36 +0200

    hasta el nabo

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
  </head>
  <body>
     <div class="container">
-        <h1>Generador aleatorio de Hola Mundos (mas)</h1>
+        <h1>Generador aleatorio de Hola Mundos (no puedo)</h1>
         <form method="post">
             <button type="submit">Generar</button>
         </form>
